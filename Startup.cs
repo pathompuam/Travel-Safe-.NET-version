4e426532025ef79bb98fb3ac12c474f672073c5a
pathompuam palasak <134137342+pathompuam@users.noreply.github.com>
2025-11-10 21:27:26 +0700

    lastest files 2

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Travel_Safe.Startup))]
+namespace Travel_Safe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
